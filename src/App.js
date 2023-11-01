4fafc2511a73f46b51ce665f6f78a8d86f3c9193
Jackson <jackson.cutler@gmail.com>
2023-10-31 17:01:55 -0700

    Configure firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './util/firebaseConfig';
 import Login from './Login';
 import ScraperForm from './ScaperForm';
 import './App.css';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const app = initializeApp(firebaseConfig);
+  console.log(app);
 
   const loginHandler = (email, password) => {
     console.log(email, password);
---
+++ src/util/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default firebaseConfig = {
+  apiKey: 'AIzaSyDRgVEFA_sEuqRcupZ3YHw6meLjYHdsDaA',
+  authDomain: 'sales-scraper-8a0c6.firebaseapp.com',
+  projectId: 'sales-scraper-8a0c6',
+  storageBucket: 'sales-scraper-8a0c6.appspot.com',
+  messagingSenderId: '474704152889',
+  appId: '1:474704152889:web:13eff93a95e185559800e8',
+};
